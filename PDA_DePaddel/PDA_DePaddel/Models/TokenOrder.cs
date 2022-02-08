d6ddf949b59ed7a6ccf82605d0fd2e37ee039a2d
Debeah <robbe.decapmaker@hotmail.be>
2022-02-08 15:48:26 +0100

    Buttons for TokenOrders

--- PDA_DePaddel/PDA_DePaddel/Models/TokenOrder.cs
+++ PDA_DePaddel/PDA_DePaddel/Models/TokenOrder.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace PDA_DePaddel.Models
 {
-   public class TokenOrder
+   public class TokenOrder : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
         public Guid ID { get; set; } 
         public decimal Price { get; set; }
         public string Comment { get; set; }
